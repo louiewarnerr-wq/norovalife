90047400cb2e1ec141b2ef8300b49a1dbb5214d4
louiewarnerr-wq <louiewarnerr@gmail.com>
2026-02-16 20:07:52 +0000

    Add list of countries to countries.js

---
+++ countries.js
@@ -0,0 +1,8 @@
+window.NOROVA_COUNTRIES = [
+  "United Kingdom","United States","Canada","Australia","Ireland","France","Germany","Spain","Italy","Netherlands",
+  "Sweden","Norway","Denmark","Switzerland","Poland","Portugal","Romania","Greece","Turkey","Ukraine",
+  "Brazil","Mexico","Argentina","Chile","Colombia","Peru","Venezuela",
+  "Nigeria","South Africa","Kenya","Egypt","Morocco","Tunisia",
+  "India","Pakistan","Bangladesh","China","Japan","South Korea","Thailand","Vietnam","Philippines","Indonesia",
+  "United Arab Emirates","Saudi Arabia","Qatar","Israel"
+];
